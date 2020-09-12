2a6ce2695971480cbef1ff806c12efcfadca2c77
SaqibAleem <saqib.py@gmail.com>
2020-09-12 19:53:30 +0500

    added message of telegram

--- functions/filter_count_documents.py
+++ functions/filter_count_documents.py
@@ -4,7 +4,19 @@ from os import getenv
 
 def count_filter_document():
     client=pymongo.MongoClient(getenv("FILTERS_MONGODB_URI"))
+    total_doc=client['Hifi'].records.count_documents()
     document_count=client['Hifi'].records.count_documents({'status':{'$exists':False}})
+    API=getenv("TELEGRAM_MESSAGE_API")
+    
+    API=API.format(
+        chat_id=getenv("TELEGRAM_GROUP_CHAT_ID"),
+        message="total documents : "+str(total_doc)+"\n"
+                +"Non-empty Docs : "+str(document_count)
+        )
+
+    requests.get(API)
     return {
         'status':200,
-        'document_count':document_count}
+        'document_count':document_count,
+        'total_documents':total_doc
+        }
